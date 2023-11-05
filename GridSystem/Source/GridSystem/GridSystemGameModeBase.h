f38f43b274fe84b0fa15e954228885bb59afb63c
Gauthier Bienvenu <BreizhBienvenu@gmail.com>
2023-11-05 17:10:45 +0100

    First push - UE project

---
+++ GridSystem/Source/GridSystem/GridSystemGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GridSystemGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRIDSYSTEM_API AGridSystemGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
